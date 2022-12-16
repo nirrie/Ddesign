062591cba1356bbd8439c1bc500b37496448d728
nirrie <97035721@st.deltion.nl>
2022-12-16 15:49:34 +0100

    Ik heb in de session controller de request my_name veranderd naar ip_address zodat het de juiste sessiedata opslaat

--- app/Http/Controllers/SesssionController.php
+++ app/Http/Controllers/SesssionController.php
@@ -6,17 +6,17 @@ use Illuminate\Http\Request;
 
    class SessionController extends Controller {
         public function accessSessionData(Request $request) {
-           if($request->session()->has('my_name'))
-              echo $request->session()->get('my_name');
+           if($request->session()->has('ip_address'))
+              echo $request->session()->get('ip_address');
            else
               echo 'No data in the session';
         }
         public function storeSessionData(Request $request) {
-           $request->session()->put('my_name');
+           $request->session()->put('ip_address');
            echo "Data has been added to session";
         }
         public function deleteSessionData(Request $request) {
-           $request->session()->forget('my_name');
+           $request->session()->forget('ip_address');
            echo "Data has been removed from session.";
         }
      }
